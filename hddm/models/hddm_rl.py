b30e99be111ac42388354c078b3eb8ee60365dcd
jungsunyoo <yoojungsun0@gmail.com>
2022-04-16 14:26:22 -0700

    debug

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -612,8 +612,8 @@ class HDDMrl(HDDM):
             wfpt_parents['window_start'] = -1.00
             wfpt_parents['window_size'] = -1.00
         else:
-            wfpt_parents['window_start'] = self.window_start.astype(float)
-            wfpt_parents['window_size'] = self.window_size.astype(float)
+            wfpt_parents['window_start'] = self.window_start
+            wfpt_parents['window_size'] = self.window_size
 
         # wfpt_parents["z"] = knodes["z_bottom"] if "z" in self.include else 0.5
 
