2c8986b95f268ca18aef5349efce85a085d0e900
uciccnl@gmail.com <uciccnl@gmail.com>
2023-01-27 13:21:29 -0800

    fixing error

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -772,19 +772,19 @@ def wiener_like_rl_2step(np.ndarray[double, ndim=1] x1, # 1st-stage RT
                       double v_2,
                       double alpha2,
                       double w, double window_start, double window_size,
-                      sv, sz, st, sv2, sz2, st2, 
+                      double sv=0, 
+                      double sz=0, 
+                      double st=0, 
+                      double sv2=0, 
+                      double sz2=0, 
+                      double st2=0, 
 
 
                       double err, int n_st=10, int n_sz=10, bint use_adaptive=1, double simps_err=1e-8,
                       double p_outlier=0, double w_outlier=0,
                       ):
 
-    cdef double sz = 0
-    cdef double st = 0
-    cdef double sv = 0
-    cdef double sz2 = 0
-    cdef double st2 = 0
-    cdef double sv2 = 0
+
 
     cdef Py_ssize_t size = x1.shape[0]
     cdef Py_ssize_t i, j
